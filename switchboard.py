25741ffa9839dd7b5826b81e12c331ee09b23e2c
Thomas J Fan <thomasjpfan@gmail.com>
2020-05-10 14:51:45 -0400

    REL Init commit

---
+++ switchboard.py
@@ -0,0 +1,56 @@
+from texasbbq import main, GitSource, GitTarget
+
+
+class ScikitLearnSource(GitSource):
+
+    module = __name__
+
+    @property
+    def name(self):
+        return "scikit-learn"
+
+    @property
+    def clone_url(self):
+        return "https://github.com/scikit-learn/scikit-learn"
+
+    @property
+    def git_ref(self):
+        return "master"
+
+    @property
+    def install_command(self):
+        return "pip install -e ."
+
+    @property
+    def conda_dependencies(self):
+        return []
+
+
+class ImbalanceLearnTests(GitTarget):
+    @property
+    def name(self):
+        return "imbalanced-learn"
+
+    @property
+    def clone_url(self):
+        return "https://github.com/scikit-learn-contrib/imbalanced-learn"
+
+    @property
+    def git_ref(self):
+        return "master"
+
+    @property
+    def install_command(self):
+        return "pip install -e ."
+
+    @property
+    def test_command(self):
+        return "pytest -v imblearn"
+
+    @property
+    def conda_dependencies(self):
+        return []
+
+
+if __name__ == "__main__":
+    main(ScikitLearnSource())
